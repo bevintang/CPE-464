c0ecf42197e2620f90465b35de8e78d437119e81
Bevin Tang <bevintang@gmail.com>
2018-01-21 00:19:58 -0800

    Finished file import

--- program1/trace.c
+++ program1/trace.c
@@ -1,14 +1,36 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <pcap.h>
 
-int main (int argc, char* argv[]){
-	// Check for proper number of arguments
+void checkArgs(int argc){
 	if (argc != 2){
 		fprintf(stderr, "Usage: trace TraceFile.pcap\n");
-		return -1;
+		exit(EXIT_FAILURE);
 	}
+}
+
+pcap_t* openPcapFile(const char* fname){
+	char errbuf[PCAP_ERRBUF_SIZE];
+	pcap_t* result = pcap_open_offline(fname, errbuf);
+	if (result == NULL){
+		fprintf(stderr, "file does not exist\n");
+		exit(EXIT_FAILURE);
+	}
+	return result;
+}
+
+int main (int argc, char* argv[]){
+	pcap_t* pcap_file;
+	struct pcap_pkthdr** pkt_header;
+	const u_char** pkt_data;
+	const char* fname;
+
+	checkArgs(argc);				// Check for proper number of arguments
+	fname = argv[1];				//grab file name
+	pcap_file = openPcapFile(fname);
 
 	// Open pcap file
+
 	
 	return 0;
 }
