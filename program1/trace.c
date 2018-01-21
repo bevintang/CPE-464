20d2278762cfb398051a4a0f7ddee5c6cb26ece9
Bevin Tang <bevintang@gmail.com>
2018-01-21 01:05:57 -0800

    copied some files, now check for appropriate files

--- program1/trace.c
+++ program1/trace.c
@@ -1,7 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <pcap.h>
 
+/* Helper -- Check valid number of arguments */
 void checkArgs(int argc){
 	if (argc != 2){
 		fprintf(stderr, "Usage: trace TraceFile.pcap\n");
@@ -9,9 +11,18 @@ void checkArgs(int argc){
 	}
 }
 
+/* Helper -- Open a PCap File */
 pcap_t* openPcapFile(const char* fname){
 	char errbuf[PCAP_ERRBUF_SIZE];
 	pcap_t* result = pcap_open_offline(fname, errbuf);
+
+    // check extension for .pcap file
+    if (!strstr(fname, ".pcap") != NULL){
+        fprintf(stderr, "not a pcap file\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // try to open file
 	if (result == NULL){
 		fprintf(stderr, "file does not exist\n");
 		exit(EXIT_FAILURE);
@@ -25,11 +36,10 @@ int main (int argc, char* argv[]){
 	const u_char** pkt_data;
 	const char* fname;
 
-	checkArgs(argc);				// Check for proper number of arguments
-	fname = argv[1];				//grab file name
-	pcap_file = openPcapFile(fname);
 
-	// Open pcap file
+	checkArgs(argc);                   // Check for proper number of arguments
+	fname = argv[1];
+	pcap_file = openPcapFile(fname);   // open pcap file
 
 	
 	return 0;
