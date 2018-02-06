512d1bab4c2e52f21ab2e1e1a03330aae4c7c935
Bevin Tang <bevintang@bevins-air.home>
2018-02-05 23:31:21 -0800

    Completed new client handling. Developing %M -- not ready to compile

--- program2/source/myClient.c
+++ program2/source/myClient.c
@@ -21,59 +21,181 @@
 
 #define MAXBUF 1400
 #define DEBUG_FLAG 1
+#define MESSAGE 5	// flag = 5
+#define BLOCK 69
+#define UNBLOCK 70
+#define LIST 10		// flag = 10
+#define EXIT 8		// flag = 8
+#define NEW_CLIENT 69
 #define xstr(a) str(a)
 #define str(a) #a
 
-void sendToServer(int socketNum);
+void sendToServer(int socketNum, uint8_t* sendBuf, int sendLen, int sendFlag);
 void checkArgs(int argc, char * argv[]);
+
+void attachChatHeader(uint8_t* sendBuf, uint16_t size, uint8_t flag) {
+	uint16_t trueSize = htons(size + 3);
+	sendBuf[0] = ((trueSize & 0xff00) >> 8);	// insert leading byte
+	sendBuf[1] = trueSize & 0x00ff;				// insert trailing byte
+	sendBuf[2] = flag;
+}
+
+void initConnection(int socketNum, char** argv, uint8_t* sendBuf) {
+	attachChatHeader(sendBuf, strlen(argv[1]) + 3, NEW_CLIENT);
+	memcpy(sendBuf+3, argv[1], strlen(argv[1]));
+	sendToServer(socketNum, sendBuf, strlen(argv[1]) + 3, 0);
+}
+
+int parseID(char* token) {
+	int returnVal = -1;
+
+	if (memcmp(token, "%M", 2) == 0 || memcmp(token, "%m", 2) == 0) {
+		returnVal = MESSAGE;
+	}
+	else if (memcmp(token, "%B", 2) == 0 || memcmp(token, "%b", 2) == 0) {
+		returnVal = BLOCK;
+	}
+	else if (memcmp(token, "%U", 2) == 0 || memcmp(token, "%u", 2) == 0) {
+		returnVal = UNBLOCK;
+	}
+	else if (memcmp(token, "%L", 2) == 0 || memcmp(token, "%l", 2) == 0) {
+		returnVal = LIST;
+	}
+	else if (memcmp(token, "%E", 2) == 0 || memcmp(token, "%e", 2) == 0) {
+		returnVal = EXIT;
+	}
+
+	return returnVal;
+}
+
+uint16_t getPacketLength(char** tokens, int numTokens) {
+	uint16_t totalLength = 0;
+	int i = 0;
+
+	for (i = 0; i < numTokens; i++) {
+		totalLength += strlen(tokens[i]);
+	}
+	return totalLength;
+}
+
+void sendMessage(uint8_t* sendBuf, char* input, int socketNum, uint8_t* thisHandle) {
+	uint16_t totalLength = 0;
+	int bufPos = 3;		// position after chat-header
+	int curLength = 0;	// length of each token
+	char* tokens[30];
+	int i = 0, numTokens = 0;
+
+	// Split input into %M tokens -- making sure to leave 
+	tokens[0] = strtok(input, " ");
+	for (i = 0; i < 2; i++) {
+		tokens[i] = strtok(NULL, " ");	// grab command and number of handles
+	}
+	/*
+		I STOPPED RIGHT HERE BEFORE SHOWERING
+	*/
+
+	totalLength = getPacketLength(tokens, numTokens);
+
+	// Setup sendBuf for Message Send
+	attachChatHeader(sendBuf, totalLength, (uint8_t)MESSAGE);
+	for (i = 0; i < numTokens; i++) {
+		curLength = strlen(tokens[i]);
+		memcpy(sendBuf+bufPos, tokens[i], curLength);
+		bufPos += curLength;
+		printf("\tClient side String: %s\n", tokens[i]);
+		printf("\tLast letter in buf: %c\n", sendBuf[bufPos-1]);
+	}
+
+	sendToServer(socketNum, sendBuf, totalLength, 0);
+}
+
+void parseCommand(char* input, uint8_t* sendBuf, int socketNum, uint8_t* thisHandle) {
+	int commandID = -1;
+	char command[2];
+	memcpy(command, input, 2);
+
+	// // Fill token array
+	// while (tokens[i] != NULL) {
+	// 	++i;
+	// 	tokens[i] = strtok(NULL, " ");
+	// }
+	//numTokens = i;
+
+
+	bzero(sendBuf, MAXBUF);
+	commandID = parseID(command);
+	switch (commandID) {
+		case MESSAGE:
+			printf("\tMessage command!\n");
+			sendMessage(sendBuf, input, socketNum, thisHandle);
+			break;
+		case BLOCK:
+			printf("\tBlock command!\n");
+			break;
+		case UNBLOCK:
+			printf("\tUnblock command!\n");
+			break;
+		case LIST:
+			printf("\tList command!\n");
+			break;
+		case EXIT:
+			printf("\tExit command!\n");
+			break;
+		case -1:
+			printf("\tCommand not recognized\n");
+			break;
+	}
+}
+
+void run (int socketNum, uint8_t* sendBuf, uint8_t* thisHandle) {
+	char input[MAXBUF];
+	//int sendLen = 0;        	// amount of data to send
+
+	while (memcmp(sendBuf, "%E", 2) != 0 && memcmp(sendBuf, "%e", 2) != 0){
+		bzero(input, MAXBUF);
+		printf("$: ");
+		scanf(" %" xstr(MAXBUF) "[^\n]%*[^\n]", input);
+		parseCommand(input, sendBuf, socketNum, thisHandle);
+	}
+	send(socketNum, sendBuf, -1, 0);
+}
 
 int main(int argc, char * argv[])
 {
-	int socketNum = 0;         //socket descriptor
+	int socketNum = 0;
+	uint8_t sendBuf[MAXBUF];   	// data buffer
+	checkArgs(argc, argv);
+	uint8_t thisHandle[100];
 
-	/* set up the TCP Client socket  */
-	socketNum = tcpClientSetup(argv[1], argv[2], DEBUG_FLAG);
-	
-	sendToServer(socketNum);
-	
+	socketNum = tcpClientSetup(argv[2], argv[3], DEBUG_FLAG);
+	initConnection(socketNum, argv, sendBuf);
+	memcpy(handle, argv[1], strlen(argv[1]));
+	run(socketNum, sendBuf, thisHandle);
 	close(socketNum);
 	
 	return 0;
 }
 
-void sendToServer(int socketNum)
+void sendToServer(int socketNum, uint8_t* sendBuf, int sendLen, int sendFlag)
 {
-	char sendBuf[MAXBUF];   // data buffer
-	int sendLen = 0;        // amount of data to send
-	int sent = 0;           // actual amount of data sent/* get the data and send it   */
+	int sent = 0;	// actual amount of data sent
 
-	while (memcmp(sendBuf, "%E", 2) != 0 && memcmp(sendBuf, "%e", 2) != 0){
-		printf("$: ");
-		scanf(" %" xstr(MAXBUF) "[^\n]%*[^\n]", sendBuf);
-		
-		sendLen = strlen(sendBuf) + 1;
-		printf("read: %s len: %d\n", sendBuf, sendLen);
-			
-		sent =  send(socketNum, sendBuf, sendLen, 0);
-		if (sent < 0)
-		{
-			perror("send call");
-			exit(-1);
-		}
-
-		printf("String sent: %s \n", sendBuf);
-		printf("Amount of data sent is: %d\n", sent);
+	sent = send(socketNum, sendBuf, sendLen+1, 0);
+	if (sent < 0)
+	{
+		perror("send call");
+		exit(-1); 
 	}
-	bzero(sendBuf, MAXBUF);
-	send(socketNum, sendBuf, -1, 0);
+	printf("\tString sent: %s\n", sendBuf+3);
+	printf("\tAmount of data sent: %d\n", sent);
 }
 
 void checkArgs(int argc, char * argv[])
 {
 	/* check command line arguments  */
-	if (argc != 3)
+	if (argc != 4)
 	{
-		printf("usage: %s host-name port-number \n", argv[0]);
+		printf("usage: %s handle-name host-name port-number \n", argv[0]);
 		exit(1);
 	}
 }
--- program2/source/myServer.c
+++ program2/source/myServer.c
@@ -21,15 +21,19 @@
 #include "networks.h"
 
 #define MAXBUF 1400
-#define DEBUG_FLAG 1
-
-void recvFromClient(int clientSocket, char* buf);
-int checkArgs(int argc, char *argv[]);
+#define MESSAGE 5	// flag = 5
+#define LIST 10		// flag = 10
+#define EXIT 8		// flag = 8
+#define NEW_CLIENT 69
+#define DEBUG_FLAG 1 
 
 typedef struct {
 	int fd;
 	char handle[100];
 } Client;
+
+void recvFromClient(Client* clients, int clientSocket, uint8_t* buf);
+int checkArgs(int argc, char *argv[]);
 
 void error(char* message) {
 	printf("%s\n", message);
@@ -75,21 +79,77 @@ void checkNewClient(int serverSocket, fd_set* fds, int* numClients, int* maxFd,
 	}
 }
 
-int checkExitCommand(char* buf, int* numClients, int clientSocket) {
+int checkExitCommand(uint8_t* buf, int* numClients, int clientSocket, Client* clients) {
+	Client nullClient;
+
 	if (memcmp(buf, "%E", 2) != 0 && memcmp(buf, "%e", 2) != 0){
 		return 0;
 	}
 	// Case client has quit
-	close(clientSocket);		// close socket
-	// free entry in handle array
+	nullClient.fd = 0;
+	clients[clientSocket] = nullClient;	// free entry in handle array
 	(* numClients) = (* numClients) - 1;
-	printf("Client at socket %d closed\n", clientSocket);
+	close(clientSocket);
+	printf("\tClient at socket %d closed\n", clientSocket);
 	return 1;
+}
+
+void setName (Client* clients, int clientSocket, uint8_t* buf) {
+	uint16_t nameSize = ntohs(( ((uint16_t)buf[0] << 8) | buf[1])) - 3;
+	memcpy(clients[clientSocket].handle, buf+3, nameSize);
+	printf("Client at socket %d's name set to %s\n", clientSocket, clients[clientSocket].handle);
+}
+
+void parseCommand(Client* clients, int clientSocket, uint8_t* buf) {
+	uint8_t commandID = buf[2];
+
+	switch (commandID) {
+		case MESSAGE:
+			printf("\tIncoming Message!\n");
+			break;
+		case LIST:
+			printf("\tClient Requests List of handles!\n");
+			break;
+		case EXIT:
+			printf("\tClient wants to exit!\n");
+			break;
+		case NEW_CLIENT:
+			printf("\tNew client!\n");
+			setName(clients, clientSocket, buf);
+			break;
+	}
+}
+
+void recvFromClient(Client* clients, int clientSocket, uint8_t* buf)
+{
+	int messageLen = 0;
+	
+	if ((messageLen = recv(clientSocket, buf, MAXBUF, 0)) < 0)
+	{
+		perror("recv call");
+		exit(-1);
+	}
+
+	parseCommand(clients, clientSocket, buf);
+
+	// Display buffer contents
+	printf("Message received, length: %d Data: %s\n", messageLen, buf+3);
+	printf("Current contents of buf: %s\n\n", buf+3);
+}
+
+void recvRequest(int clientSocket, fd_set* fds, uint8_t* buf, int* numClients,
+		Client* clients) {
+
+	if (FD_ISSET(clientSocket, fds)) {
+		bzero(buf, MAXBUF);
+		recvFromClient(clients, clientSocket, buf);
+		checkExitCommand(buf, numClients, clientSocket, clients);	
+	}
 }
 
 /* Checks to see if clients have any pending requests */
 void checkClients(fd_set* fds, int* numClients, Client* clients, int* maxFd, 
-		char* buf) {
+		uint8_t* buf) {
 	int clientSocket = 0;
 	int i = 0;
 	for(i = 3; i < *maxFd + 1; i++) {
@@ -97,10 +157,7 @@ void checkClients(fd_set* fds, int* numClients, Client* clients, int* maxFd,
 			continue;	
 		}
 		clientSocket = clients[i].fd;
-		if (FD_ISSET(clientSocket, fds)) {
-			recvFromClient(clientSocket, buf);
-			checkExitCommand(buf, numClients, clientSocket);	
-		}
+		recvRequest(clientSocket, fds, buf, numClients, clients);
 	}
 }
 
@@ -113,7 +170,7 @@ void checkClients(fd_set* fds, int* numClients, Client* clients, int* maxFd,
 		5) look for new clients
 		6) check for client requests
 	} */
-void run(int serverSocket, int portNumber, char* buf, int* numClients, 
+void run(int serverSocket, int portNumber, uint8_t* buf, int* numClients, 
 		Client* clients, int* maxFd) {
 	fd_set fds;
 
@@ -127,35 +184,6 @@ void run(int serverSocket, int portNumber, char* buf, int* numClients,
 		checkNewClient(serverSocket, &fds, numClients, maxFd, clients);
 		checkClients(&fds, numClients, clients, maxFd, buf);
 	}
-}
-
-int main(int argc, char *argv[])
-{
-	int serverSocket = 0, portNumber = 0;
-	int numClients = 0, maxFd = 0;
-	char buf[MAXBUF];
-	Client* clients = (Client* ) calloc(10, sizeof(Client));
-	
-	portNumber = checkArgs(argc, argv);			// get port number
-	serverSocket = tcpServerSetup(portNumber);	// create server socket
-	maxFd = serverSocket;
-	run(serverSocket, portNumber, buf, &numClients, clients, &maxFd);	
-	close(serverSocket);
-	return 0;
-}
-
-void recvFromClient(int clientSocket, char* buf)
-{
-	int messageLen = 0;
-	
-	if ((messageLen = recv(clientSocket, buf, MAXBUF, 0)) < 0)
-	{
-		perror("recv call");
-		exit(-1);
-	}
-	//now get the data from the client_socket
-	printf("Message received, length: %d Data: %s\n", messageLen, buf);
-	printf("Current contents of buf: %c%c\n", buf[0], buf[1]);
 }
 
 int checkArgs(int argc, char *argv[])
@@ -177,3 +205,17 @@ int checkArgs(int argc, char *argv[])
 	return portNumber;
 }
 
+int main(int argc, char *argv[])
+{
+	int serverSocket = 0, portNumber = 0;
+	int numClients = 0, maxFd = 0;
+	uint8_t buf[MAXBUF];
+	Client* clients = (Client* ) calloc(10, sizeof(Client));
+	
+	portNumber = checkArgs(argc, argv);			// get port number
+	serverSocket = tcpServerSetup(portNumber);	// create server socket
+	maxFd = serverSocket;
+	run(serverSocket, portNumber, buf, &numClients, clients, &maxFd);	
+	close(serverSocket);
+	return 0;
+}
